1c7aa833183835f5b8b833713b9f505e7894f65d
Carlos Zuniga <carlos.zuniganajaarro@outlook.com>
2026-04-22 02:03:43 -0700

    imported matplotlib and made a chart, just making sure that it worked, obviously the data from the chart isn't relevant because it still counts the win rate of heroes with very few matches

--- analysis.py
+++ analysis.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 df = pd.read_csv("data/marvel_rivals_stats.csv")
 
-print(df)
+#this is just me making sure that matplotlib works and I can make a chart
+top = df.sort_values(by="win_rate", ascending=False).head(15)
+
+plt.title("Top Heroes by Win Rate", fontsize=15, fontfamily="Courier", color= "blue")
+plt.barh(top["hero"], top["win_rate"], color="green")
+plt.xlabel("Win Rate", fontsize=15, fontfamily="Courier", color="red")
+plt.ylabel("Hero", fontsize = 15, fontfamily="Courier", color="green")
+plt.tight_layout()
+plt.show()
