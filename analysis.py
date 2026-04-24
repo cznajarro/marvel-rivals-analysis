760bca36cf13444b1991184002b5b9904ad2a432
Carlos Zuniga <carlos.zuniganajaarro@outlook.com>
2026-04-23 20:14:15 -0700

    added a scatter plot that looks at the win rates of my characters vs their k/d/a's and it's grouped by their roles.

--- analysis.py
+++ analysis.py
@@ -16,3 +16,27 @@ plt.tight_layout()
 
 
 plt.show()
+
+
+vanguards = df[(df["matches"]>=10.0) & (df["role"] == "Vanguard")]
+vanguards_kda = vanguards["K/D/A"]
+vanguards_winrate = vanguards["win_rate"]
+
+duelists = df[(df["matches"]>=10.0) & (df["role"] == "Duelist")]
+duelists_kda = duelists["K/D/A"]
+duelists_winrate = duelists["win_rate"]
+
+strategists = df[(df["matches"]>=10.0) & (df["role"] == "Strategist")]
+strategists_kda = strategists["K/D/A"]
+strategists_winrate = strategists["win_rate"]
+
+
+
+plt.title("Win Rate by K/D/A", size=20, fontweight="bold", loc="left")
+plt.xlabel("K/D/A", fontweight="bold")
+plt.ylabel("Win Rate", fontweight="bold")
+
+plt.scatter(vanguards_kda, vanguards_winrate, color="blue")
+plt.scatter(duelists_kda, duelists_winrate, color="red")
+plt.scatter(strategists_kda, strategists_winrate, color="green")
+plt.show()
